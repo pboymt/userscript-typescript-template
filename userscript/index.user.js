fef9bb2adf36ce9e056088815b1873c14d7349b3
pboymt <10893582+pboymt@users.noreply.github.com>
2023-04-19 12:48:01 +0800

    chore: Bump version of this template.

--- userscript/index.user.js
+++ userscript/index.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name userscript-typescript-template
-// @version 1.1.0
+// @version 1.1.1
 // @namespace http://tampermonkey.net/
 // @description Template repo using Webpack and TypeScript to build your userscript for Tampermonkey and more extensions.
 // @homepage https://github.com/pboymt/userscript-typescript-template#readme
+// @license https://opensource.org/licenses/MIT
 // @match https://github.com*
-// @require https://cdn.jsdelivr.net/npm/$axios@$0.27.2
+// @require https://cdn.jsdelivr.net/npm/axios@0.27.2
 // ==/UserScript==
 
 /******/ (() => { // webpackBootstrap
